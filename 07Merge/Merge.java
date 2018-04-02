7f075cfb4d3cf745cc7d2a9417565dbb046ef69a
rkhuda <rkhuda@stuy.edu>
2018-04-02 01:22:47 -0400

    forgot to commit mergesort folder

---
+++ 07Merge/Merge.java
@@ -0,0 +1,20 @@
+public class Merge {
+
+    public static void mergesort(int[] data){
+	int[] temp = new int[data.length];
+    }
+
+    private static void msort(int[] data, int[] temp, int lo, int hi){
+	if (lo >= hi){
+	    return;
+	}
+	for (int x = lo; x <= hi; x++){
+	    temp[x] = data[x];
+	}
+	int mid = (hi + lo)/2;
+    }
+
+    private static void merge(int[] data, int[] temp, int lo, int mid, int hi){
+	//do something
+    }
+}
